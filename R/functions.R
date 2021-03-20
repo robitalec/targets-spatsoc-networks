bcef384359aeb315a3a5aea465a37697fb558665
Alec L. Robitaille <robit.alec@gmail.com>
2021-03-20 01:38:35 -0230

    add prep_dates function

---
+++ R/functions.R
@@ -0,0 +1,83 @@
+#' Prepare datetime column
+#'
+#' @param DT data.table
+#' @param datetime character; datetime column name
+#' @param tz character; time zone.
+#'
+#' @return
+#' data.table with datetime columns added
+#' @export
+#'
+#' @examples
+#' path <- system.file('extdata', 'DT.csv', package = 'spatsoc')
+#' DT <- read_data(path = path)
+#' prep_dates(DT, 'datetime', 'Canada/Newfoundland')
+prep_dates <- function(DT, datetime, tz) {
+  check_truelength(DT)
+  check_col(DT, datetime, 'datetime')
+
+  if (missing(tz)) {
+    stop('must provide a tz argument')
+  }
+
+  DT[, datetime := anytime::anytime(.SD[[1]], tz = tz, asUTC = TRUE), .SDcols = datetime]
+
+  DT[, doy := data.table::yday(datetime)]
+  DT[, yr := data.table::year(datetime)]
+  DT[, mnth := data.table::month(datetime)]
+}
+
+
+
+
+
+# Etc ---------------------------------------------------------------------
+#' check col
+#' @param DT data.table
+#' @param col column name
+#' @param arg argument name
+#' @param extra extras
+check_col <- function(DT = NULL, col = NULL, arg = NULL, extra = NULL) {
+
+  if (is.null(arg)) {
+    it <- col
+  } else {
+    it <- paste0(arg, " ('", col, "')")
+  }
+
+  if (!(col %in% colnames(DT))) {
+    stop(paste0(it, ' column not found in DT', extra))
+  }
+}
+
+#' check type
+#' @param DT data.table.
+#' @param col column name.
+#' @param type typeof type type typeof type.
+check_type <- function(DT = NULL, col = NULL, type = NULL) {
+  if (!(typeof(DT[[col]]) %in% type)) {
+    stop(paste0(col, ' does not match required type: ', type))
+  }
+}
+
+
+#' overwite_col
+#' @param DT data.table
+#' @param col column name
+overwrite_col <- function(DT = NULL, col = NULL) {
+
+  if (col %in% colnames(DT)) {
+    warning(paste0('overwriting ', col, ' column'))
+    set(DT, j = eval(col), value = NULL)
+  }
+}
+
+#' check_truelength
+#' @param DT data.table
+check_truelength <- function(DT) {
+  if (truelength(DT) == 0) {
+    stop('please run data.table::alloc.col on your DT to allocate columns')
+  }
+}
+
+
