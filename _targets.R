93790ab9b028fe058efb7bfe7257778167a3065f
Alec L. Robitaille <robit.alec@gmail.com>
2021-03-30 00:52:30 -0230

    static option

--- _targets.R
+++ _targets.R
@@ -17,7 +17,8 @@ source('R/functions.R')
 
 
 # Targets options ---------------------------------------------------------
-tar_option_set(format = "qs")
+tar_option_set(format = "qs",
+               error = 'workspace')
 
 
 
@@ -43,57 +44,104 @@ weighted <- TRUE
 
 
 # Targets -----------------------------------------------------------------
-c(
-  tar_files(
-    paths,
-    dir(input, '.csv', full.names = TRUE)
+# c(
+#   tar_files(
+#     paths,
+#     dir(input, '.csv', full.names = TRUE)
+#     ),
+#
+#   tar_target(
+#     locs,
+#     fread(paths),
+#     map(paths)
+#   ),
+#
+#   tar_target(
+#     prep,
+#     prep_dates(locs, datetime, tz),
+#     map(locs)
+#   ),
+#
+#   tar_target(
+#     timegroups,
+#     group_times(prep, datetime, tempthresh),
+#     map(prep)
+#   ),
+#
+#   tar_target(
+#     spatgroups,
+#     group_pts(timegroups, spatthresh, id, coords, 'timegroup'),
+#     map(timegroups)
+#   ),
+#
+#   tar_target(
+#     gbi,
+#     get_gbi(DT = spatgroups, group = group, id = id),
+#     map(spatgroups)
+#   ),
+#
+#   tar_target(
+#     networks,
+#     get_network(gbi, data_format = 'GBI', association_index = associationindex),
+#     map(gbi)
+#   ),
+#
+#   tar_target(
+#     graphs,
+#     graph.adjacency(networks, mode = mode, diag = diag, weighted = weighted),
+#     map(networks)
+#   ),
+#
+#   tar_target(
+#     metrics,
+#     calc_metrics(graphs),
+#     map(graphs)
+#   ),
+#
+#   # randomizations
+# )
+tar_map(
+  values = list(path = dir(input, '.csv', full.names = TRUE)),
+  c(
+    tar_target(
+      locs,
+      fread(path)
     ),
 
-  tar_target(
-    locs,
-    fread(paths),
-    map(paths)
-  ),
+    tar_target(
+      prep,
+      prep_dates(locs, datetime, tz)
+    ),
 
-  tar_target(
-    prep,
-    prep_dates(locs, datetime, tz),
-    map(locs)
-  ),
+    tar_target(
+      timegroups,
+      group_times(prep, datetime, tempthresh)
+    ),
 
-  tar_target(
-    timegroups,
-    group_times(prep, datetime, tempthresh),
-    map(prep)
-  ),
+    tar_target(
+      spatgroups,
+      group_pts(timegroups, spatthresh, id, coords, 'timegroup')
+    ),
 
-  tar_target(
-    spatgroups,
-    group_pts(timegroups, spatthresh, id, coords, 'timegroup'),
-    map(timegroups)
-  ),
+    tar_target(
+      gbi,
+      get_gbi(DT = spatgroups, group = group, id = id)
+    ),
 
-  tar_target(
-    gbi,
-    get_gbi(DT = spatgroups, group = group, id = id),
-    map(spatgroups)
-  ),
+    tar_target(
+      networks,
+      get_network(gbi, data_format = 'GBI', association_index = associationindex)
+    ),
 
-  tar_target(
-    networks,
-    get_network(gbi, data_format = 'GBI', association_index = associationindex),
-    map(gbi)
-  ),
+    tar_target(
+      graphs,
+      graph.adjacency(networks, mode = mode, diag = diag, weighted = weighted)
+    ),
 
-  tar_target(
-    graphs,
-    graph.adjacency(networks, mode = mode, diag = diag, weighted = weighted),
-    map(networks)
-  ),
-
-  tar_target(
-    metrics,
-    calc_metrics(graphs),
-    map(graphs)
+    tar_target(
+      metrics,
+      calc_metrics(graphs)
+    )
   )
 )
+
