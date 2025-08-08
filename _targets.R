e257735d8bc786c166584607e7e647d277fb0685
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-08 18:06:34 -0300

    add descriptions

--- _targets.R
+++ _targets.R
@@ -49,65 +49,97 @@ splitBy <- c('yr', 'mnth')
 tar <- c(
   tar_target(
     locs,
-    fread(filepath)
+    fread(filepath),
+    description = 'fread(filepath)'
   ),
 
   tar_target(
     prep,
-    prep_dates(locs, datetime)
+    prep_dates(locs, datetime),
+    description = 'prep_dates(locs, datetime)'
   ),
 
   tar_target(
     splits,
     prep[, tar_group := .GRP, by = splitBy],
-    iteration = 'group'
+    iteration = 'group',
+    description = 'tar_group by splitBy'
   ),
 
   tar_target(
     splitsnames,
-    unique(prep[, .(filepath = filepath), by = splitBy])
+    unique(prep[, .(filepath = filepath), by = splitBy]),
+    description = 'unique(filepath) by splitBY'
   ),
 
   tar_target(
     timegroups,
-    group_times(splits, datetime, temporal_threshold),
-    map(splits)
+    group_times(
+      DT = splits,
+      datetime = datetime,
+      threshold = temporal_threshold
+    ),
+    map(splits),
+    description = 'group_times()'
   ),
 
   tar_target(
     spatgroups,
-    group_pts(timegroups, spatial_threshold, id, coords, 'timegroup'),
-    map(timegroups)
+    group_pts(
+      DT = timegroups,
+      threshold = spatial_threshold,
+      id = id,
+      coords = coords,
+      timegroup = 'timegroup'
+    ),
+    map(timegroups),
+    description = 'group_pts()'
   ),
 
   tar_target(
     gbi,
-    get_gbi(DT = spatgroups, group = group, id = id),
+    get_gbi(
+      DT = spatgroups,
+      group = group,
+      id = id
+    ),
     map(spatgroups),
-    iteration = 'list'
+    iteration = 'list',
+    description = 'get_gbi()'
   ),
 
   tar_target(
     networks,
-    get_network(gbi, data_format = 'GBI', association_index = associationindex),
+    get_network(
+      association_data = gbi,
+      data_format = 'GBI',
+      association_index = associationindex
+    ),
     map(gbi),
     iteration = 'list',
-    format = 'rds'
+    format = 'rds',
+    description = 'asnipe::get_network()'
   ),
 
   tar_target(
     graphs,
-    graph_from_adjacency_matrix(networks, mode = mode,
-                                diag = FALSE, weighted = weighted),
+    graph_from_adjacency_matrix(
+      adjmatrix = networks,
+      mode = mode,
+      diag = FALSE,
+      weighted = weighted
+    ),
     map(networks),
     iteration = 'list',
-    format = 'rds'
+    format = 'rds',
+    description = 'igraph::graph_from_adjacency_matrix()'
   ),
 
   tar_target(
     metrics,
     calc_metrics(graphs),
-    map(graphs)
+    map(graphs),
+    description = 'calc_metrics()'
   ),
 
   tar_target(
@@ -116,7 +148,8 @@ tar <- c(
           splitsnames,
           spatial_threshold = spatial_threshold,
           temporal_threshold = temporal_threshold),
-    map(metrics, splitsnames)
+    map(metrics, splitsnames),
+    description = 'output'
   )
 )
 
